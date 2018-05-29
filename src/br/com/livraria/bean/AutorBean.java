9b4a0cd5d5c4a9ffdb9493dd2ec8fcf9eab624f4
croschel <croschel000@gmail.com>
2018-05-29 11:58:43 -0300

    Change

--- src/br/com/livraria/bean/AutorBean.java
+++ src/br/com/livraria/bean/AutorBean.java
@@ -2,12 +2,13 @@
 
 package br.com.livraria.bean;
 
-import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
+import java.util.List;
 import br.com.livraria.dao.DAO;
 import br.com.livraria.modelo.Autor;
 
-@ManagedBean
+@ViewScoped
 public class AutorBean {
 	
 	private Autor autor = new Autor();
@@ -22,5 +23,9 @@ public class AutorBean {
 		new DAO<Autor>(Autor.class).adiciona(this.autor);
 		
 		this.autor = new Autor();
+	}
+	
+	public List<Autor> getAutores(){ //coloca os autores em uma lista ordenada
+		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 }
